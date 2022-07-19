13f47fa5477bdb34e8476dedd020342be4dc4d5a
Tech-sis <abilawonadeola@gmail.com>
2022-07-19 16:58:33 +0100

    Handle insetion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,33 +1,50 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * @list: pointer to the head of the list
- * Return: void
+ * insertion_sort_list - insertion sort algorithm for doubly linked list
+ * @list: pointer to array to sort
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *next, *temp;
 
-	if (list == NULL || *list == NULL)
+	listint_t *node, *tmp, *next, *tmp_next, *tmp_prev;
+	int i = 0;
+
+	if (list == NULL || (*list)->next == NULL)
 		return;
-	current = *list;
-	while (current->next != NULL)
+	node = *list;
+	node = node->next;
+	while (node != NULL)
 	{
-		next = current->next;
-		if (current->n > next->n)
+		tmp = node;
+		next = node->next;
+		tmp_next = tmp->next; /*Will be NULL for the last Node*/
+		tmp_prev = tmp->prev;
+
+		while (tmp->prev != NULL && tmp_prev->n > tmp->n)
 		{
-			temp = next->next;
-			next->next = next->next->prev;
-			next->prev = current;
-			current->next = next->next;
-			current->prev = next;
-			next->next = temp;
-			if (next->next != NULL)
-				next->next->prev = next;
-			*list = current;
+			if (tmp_next != NULL)
+				tmp_next->prev = tmp_prev;
+			tmp_prev->next = tmp_next;
+			if (tmp_prev->prev == NULL)
+			{
+				tmp_prev->prev = tmp;
+				*list = tmp;
+				tmp->prev = NULL;
+			}
+			else
+			{
+				tmp->prev = tmp_prev->prev;
+				tmp_prev->prev->next = tmp;
+				tmp_prev->prev = tmp;
+			}
+			tmp->next = tmp_prev;
+			tmp_prev = tmp->prev;
+			tmp_next = tmp->next;
+			print_list(*list);
 		}
-		current = current->next;
-		print_list(*list);
+		node = next;
+		i++;
 	}
 }
